f0677de9a82cc966b8e2b4e97aa5e979e3f7c177
Elliot Chen <elliot.chen@elliot.tw>
2024-01-17 16:26:41 +0800

    init

---
+++ src/main/java/tw/elliot/mend/MendApplication.java
@@ -0,0 +1,13 @@
+package tw.elliot.mend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MendApplication.class, args);
+    }
+
+}
